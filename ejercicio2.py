1085f69b86ba6c0520fe719f465d171d736d23e1
reyesg29 <89892263+reyesg29@users.noreply.github.com>
2021-09-03 23:56:24 -0700

    segundo commit

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+nombre=input('Introduce un nombre: ')
+instrumento = input('Itroduce el nombre de un instrumento: ')
+Pais=input('Introduce el nombre de un país: ')
+
+
+print ('Luis es hermano de ' + nombre + ' le encata tocar la ' 
++ instrumento + ' el año pasado se fue de viaje ' + Pais + ' donde toco ante personas importantes.')
---
+++ ejercicio3.py
@@ -0,0 +1,5 @@
+numpalabras=0
+
+texto = input('¿Qué planes tienes para este fin de semana?')
+palabras = texto.split()
+print('La cantidad de palabras en el texto es de: ', len(palabras))
